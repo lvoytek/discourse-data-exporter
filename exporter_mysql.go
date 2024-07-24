248cf4d83533b88d191ba366f2026d58aceb8bca
Lena Voytek <lena@voytek.dev>
2024-07-24 14:43:31 -0700

    Fix mysql exporter syntax errors

--- exporter_mysql.go
+++ exporter_mysql.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/go-sql-driver/mysql"
-	"golang.org/x/tools/go/cfg"
 )
 
 var (
@@ -15,16 +14,15 @@ var (
 
 func ConnectMySQL(serverURL string, username string, password string) error {
 	mysqlCfg := mysql.Config{
-		User:      username,
-		Passwd:    password,
-		Net:       "tcp",
-		Addr:      serverURL,
-		DBName:    "discourse",
-		parseTime: true,
+		User:   username,
+		Passwd: password,
+		Net:    "tcp",
+		Addr:   serverURL,
+		DBName: "discourse",
 	}
 
 	var err error
-	mysqlDB, err = sql.Open("mysql", cfg.FormatDSN())
+	mysqlDB, err = sql.Open("mysql", mysqlCfg.FormatDSN())
 
 	if err != nil {
 		return err
@@ -34,20 +32,20 @@ func ConnectMySQL(serverURL string, username string, password string) error {
 	mysqlDB.SetMaxOpenConns(10)
 	mysqlDB.SetMaxIdleConns(10)
 
-	err := db.Ping()
+	err = mysqlDB.Ping()
 	return err
 }
 
 func InitializeMySQLDatabase() error {
 	//Topic comments
-	_, err := mysqlDB.exec("CREATE TABLE IF NOT EXISTS comments (post_id INT PRIMARY KEY, category_slug VARCHAR NOT NULL, topic_id INT NOT NULL, creation_time DATETIME NOT NULL, update_time DATETIME NOT NULL, username VARCHAR NOT NULL)")
+	_, err := mysqlDB.Exec("CREATE TABLE IF NOT EXISTS comments (post_id INT PRIMARY KEY, category_slug VARCHAR NOT NULL, topic_id INT NOT NULL, creation_time DATETIME NOT NULL, update_time DATETIME NOT NULL, username VARCHAR NOT NULL)")
 
 	return err
 }
 
 func ExportTopicCommentsMySQL(topicComments []TopicCommentsEntry) {
 	for _, topicComment := range topicComments {
-		_, err := mysqlDB.exec("INSERT INTO comments (category_slug, topic_id, post_id, creation_time, update_time, username) VALUES (?, ?, ?, ?, ?, ?)",
+		_, err := mysqlDB.Exec("INSERT INTO comments (category_slug, topic_id, post_id, creation_time, update_time, username) VALUES (?, ?, ?, ?, ?, ?)",
 			topicComment.category_slug, topicComment.topic_id, topicComment.post_id, topicComment.creation_time, topicComment.update_time, topicComment.username)
 		if err != nil {
 			log.Printf("ExportTopicCommentsMySQL error: %v", err)
