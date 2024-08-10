67378e1d17b03280384a6d143f3d130f464b27bb
Lena Voytek <lena@voytek.dev>
2024-08-10 10:18:17 -0700

    Add csv metadata to metrics

--- metrics.go
+++ metrics.go
@@ -4,26 +4,26 @@ import "time"
 
 // Metric Data
 type TopicCommentsEntry struct {
-	category_slug   string
-	topic_id        int
-	post_id         int
-	creation_time   time.Time
-	update_time     time.Time
-	username        string
-	is_initial_post bool
+	category_slug   string    `csv:"Category Slug"`
+	topic_id        int       `csv:"Topic ID"`
+	post_id         int       `csv:"Post ID"`
+	creation_time   time.Time `csv:"Creation Time"`
+	update_time     time.Time `csv:"Last Update Time"`
+	username        string    `csv:"Creator Username"`
+	is_initial_post bool      `csv:"Is the topic's text"`
 }
 
 type TopicEditsEntry struct {
-	topic_id      int
-	edit_number   int
-	creation_time time.Time
-	username      string
+	topic_id      int       `csv:"Topic ID"`
+	edit_number   int       `csv:"Edit Number"`
+	creation_time time.Time `csv:"Creation Time"`
+	username      string    `csv:"Editor Username"`
 }
 
 // Context Data
 type UserEntry struct {
-	user_id            int
-	username           string
-	name               string
-	primary_group_name string
+	user_id            int    `csv:"User ID"`
+	username           string `csv:"Username"`
+	name               string `csv:"Name"`
+	primary_group_name string `csv:"Primary Group Name"`
 }
